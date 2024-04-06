9a52cfc0994d90e410833fb4f1bc6238d4a22ff4
orufbek01 <vohobovmansurbek0@gmial.com>
2024-04-06 14:41:02 +0300

    Orufbek

--- main/models.py
+++ main/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=55)
+
+    def __str__(self):
+        return self.name
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=55)
+    title_img = models.ImageField(upload_to='title_image/')
+    description = models.CharField(max_length=255)
+    text = models.TextField()
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    single_img = models.ImageField(upload_to='single_img/')
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    name = models.CharField(max_length=155)
+    date = models.DateTimeField(auto_now=True)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.blog.title
+
+
+class About(models.Model):
+    title = models.CharField(max_length=55)
+    description = models.CharField(max_length=155)
+    image = models.ImageField(upload_to='about_img/')
+    text = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Banner(models.Model):
+    title = models.CharField(max_length=55)
+    description = models.CharField(max_length=55)
+    image = models.ImageField(upload_to='banner_img/')
--- main/views.py
+++ main/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Blog, Comment, About, Banner
 
 
 def index_view(request):
-    return render(request, 'index.html')
+    context = {
+        'about': About.objects.all(),
+        'banner': Banner.objects.all().order_by('-id')[:2],
+    }
+    return render(request, 'index.html', context)
+
+
+def blog_view(request):
+    context = {
+        'blog': Blog.objects.all()
+    }
+    return render(request, 'blog-listings--no-sidebar.html', context)
+
+
+def single_blog_view(request, pk):
+    single_blog = Blog.objects.get(pk=pk)
+    context = {
+        'single_blog': single_blog
+    }
+    if request.method == 'POST':
+        name = request.POST['name']
+        message = request.POST['message']
+        Comment.objects.create(
+            name=name,
+            message=message,
+            blog=single_blog,
+        )
+        return redirect("single_blog_url", pk=single_blog.id)
+    return render(request, 'blog-listings--no-sidebar.html', context)
+
